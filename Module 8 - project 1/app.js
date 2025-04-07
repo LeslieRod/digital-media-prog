39f4a5ddabe5fe0599aa7910489d5a784717cad0
Leslie Rodriguez <leslie.rodriguez@nisd.net>
2025-04-07 10:05:31 -0500

    JS updated

--- Module 8 - project 1/app.js
+++ Module 8 - project 1/app.js
@@ -1,16 +1,15 @@
 const haloFace = document.querySelector('.halo');
 const hornsFace = document.querySelector('.horns');
 
-// Add event listener
 haloFace.addEventListener('click', () => {
-  if (hornsFace.classList.contains('horns')) {
+  if (haloFace.classList.contains('active')) {
     hornsFace.classList.add('active');
     haloFace.classList.remove('active');
   }
 });
 
 hornsFace.addEventListener('click', () => {
-  if (haloFace.classList.contains('halo')) {
+  if (hornsFace.classList.contains('active')) {
     haloFace.classList.add('active');
     hornsFace.classList.remove('active');
   }
